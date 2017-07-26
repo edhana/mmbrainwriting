074cfa8598ccdb1f2941a40445642b6e2412d90b
Eduardo Santos Marques <eduardo.marques@avixy.com>
2017-07-26 11:28:49 -0300

    Improved the functionality of the Idea model

--- app/idea.rb
+++ app/idea.rb
@@ -1,14 +1,27 @@
 # idea model
 require 'json'
 require 'pry'
+require 'application_helper'
 
 class Idea
   attr_accessor :name, :childs
+
+  @@ideas = []
 
   def initialize(name, childs = [])
     raise ArgumentError.new("Invalid Idea Name") unless name.instance_of?(String)
 
     @name = name
     @childs = childs
+
+    @@ideas << self
+  end
+
+  def self.count
+    @@ideas.count
+  end
+
+  def self.destroy_all
+    @@ideas.clear 
   end
 end
--- spec/idea_spec.rb
+++ spec/idea_spec.rb
@@ -2,21 +2,32 @@ require 'spec_helper'
 require 'idea'
 
 describe Idea do
+
   before(:each) do
-    @idea = Idea.new "Master Mind Meeting"
-    @idea.childs << Idea.new("Child 1")
-    @idea.childs << Idea.new("Child 2")
+    Idea.destroy_all
   end
 
-  it "creates an idea object with only the name" do
+  it "instance with only the name" do
     expect(Idea.new "Master Mind Meeting").not_to be_nil
   end
 
-  it "do not create an idea object with nil name" do
+  it "instance with nil name" do
     expect {Idea.new nil}.to raise_error(ArgumentError)
   end
 
-  it "create idea with child ideas" do
-    expect(@idea.childs.count).to be 2
+  it "returns the general idea count" do
+    Idea.new 'Idea 1'
+    expect( Idea.count ).to be == 1
+  end
+
+  describe "Idea Childs" do
+    before(:each) do
+      @idea = Idea.new "Master Mind Meeting"
+    end
+
+    it "added one to a default idea instance" do
+      @idea.childs << Idea.new("Child 1")
+      expect(@idea.childs.count).to be 1 
+    end
   end
 end
